2e6a4a1a642e03f1edddb6f0002cf0349a719287
Hien Nguyen <thetukiet@gmail.com>
2019-12-23 19:13:33 +0700

    Adding models

---
+++ app/model/access.js
@@ -0,0 +1,18 @@
+
+class Access {
+    constructor(id, urlId, accessTime) {
+        this.id = id;
+        this.urlId = urlId;
+        this.accessTime = accessTime;
+    }
+
+    getHourlyAccessTime() {
+
+    }
+
+    getHourlyAccessTimeString() {
+
+    }
+}
+
+module.exports = Access;
---
+++ app/model/url.js
@@ -0,0 +1,9 @@
+
+class Url {
+    constructor(id, value) {
+        this.id = id;
+        this.value = value;
+    }
+}
+
+module.exports = Url;
